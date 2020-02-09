7aa89641f5878fa3317fb111773c9bfdf8f03251
Rudi B. Svartveit <rudsvar@gmail.com>
2020-02-09 17:52:14 +0100

    Add more tasks to CommonTypes.hs

--- intro/CommonTypes.hs
+++ intro/CommonTypes.hs
@@ -92,3 +92,74 @@ abListOfChar = ['a', 'b']
 
 helloWorld :: String
 helloWorld = "Hello " ++ "world!"
+
+alphabet :: String
+alphabet = ['a'..'z']
+
+oneToTen :: [Int]
+oneToTen = [1 .. 10]
+
+-- Tasks
+
+-- absolute value of an int
+absolute :: Int -> Int 
+absolute x = if x < 0 then -x else x
+
+-- int with smallest value
+smallest :: Int -> Int -> Int
+smallest x y = if x < y then x else y
+
+-- incrementAllBy k xs means increment all elements by k
+incrementAllBy :: Int -> [Int] -> [Int]
+incrementAllBy _ [] = []
+incrementAllBy n (x:xs) = x+n : incrementAllBy n xs
+
+-- change the tuple structure
+changeTuple :: (Int, (Char, String)) -> ((Char, Int), String)
+changeTuple (i, (c, s)) = ((c, i), s)
+
+-- hint: show 5 = "5"
+-- "FizzBuzz" if divisible by 3 and 5
+-- "Fizz" if divisible by 3
+-- "Buzz" if divisible by 5
+-- `show x` otherwise
+fizzBuzz :: [Int] -> [String]
+fizzBuzz [] = []
+fizzBuzz (x:xs) =
+    if mod x 3 == 0 && mod x 5 == 0 then "FizzBuzz" : fizzBuzz xs
+        else if mod x 3 == 0        then "Fizz" : fizzBuzz xs
+            else if mod x 5 == 0    then "Buzz" : fizzBuzz xs
+                else show x : fizzBuzz xs
+-- Alternative solution
+-- fizzBuzz str = map fizzOrBuzz str
+--     where
+--         fizzOrBuzz :: Int -> String
+--         fizzOrBuzz x
+--             | x `mod` 3 == 0 && x `mod` 5 == 0 = "FizzBuzz"
+--             | x `mod` 3 == 0 = "Fizz"
+--             | x `mod` 5 == 0 = "Buzz"
+--             | otherwise = show x
+
+isAlphabetic :: Char -> Bool
+isAlphabetic c =
+    let isLower = 'a' <= c && c <= 'z'
+        isUpper = 'A' <= c && c <= 'Z'
+     in isLower || isUpper
+-- Alternative solution
+-- isAlphabetic c = c `elem` ['a'..'Z'] || c `elem` ['A' .. 'Z']
+
+-- hint: take, drop
+substring :: Int -> Int -> String -> String
+substring start end str = take (end - start) $ drop start str
+
+combine :: [Int] -> [Char] -> [(Int, Char)]
+combine [] _ = []
+combine _ [] = []
+combine (x:xs) (y:ys) = (x,y) : combine xs ys
+
+enumerate :: [Char] -> [(Int, Char)]
+enumerate chars = combine [0..length chars-1] chars
+
+factorial :: Int -> Int
+factorial 0 = 1
+factorial n = n * factorial (n-1)
