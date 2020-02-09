f0614ee4052413378aa1dc2eff26400eec486810
Rudi B. Svartveit <rudsvar@gmail.com>
2020-02-09 18:00:08 +0100

    Create a template for the common types tasks

---
+++ tasks/CommonTypesTasks.hs
@@ -0,0 +1,44 @@
+-- Tasks
+
+-- absolute value of an int
+absolute :: Int -> Int 
+absolute = undefined
+
+-- int with smallest value
+smallest :: Int -> Int -> Int
+smallest = undefined
+
+-- incrementAllBy k xs means increment all elements by k
+incrementAllBy :: Int -> [Int] -> [Int]
+incrementAllBy = undefined
+
+-- change the tuple structure
+changeTuple :: (Int, (Char, String)) -> ((Char, Int), String)
+changeTuple = undefined
+
+-- hint: show 5 = "5"
+-- "FizzBuzz" if divisible by 3 and 5
+-- "Fizz" if divisible by 3
+-- "Buzz" if divisible by 5
+-- `show x` otherwise
+fizzBuzz :: [Int] -> [String]
+fizzBuzz = undefined
+
+-- hint: comparison of characters based on ASCII value: e.g. 'a' < 'b'
+isAlphabetic :: Char -> Bool
+isAlphabetic = undefined
+
+-- hint: take, drop
+substring :: Int -> Int -> String -> String
+substring = undefined
+
+-- Combine two lists: combine [1,2,3] ['a', 'b', 'c'] = [(1, 'a'), (2, 'b'), (3, 'c')]
+combine :: [Int] -> [Char] -> [(Int, Char)]
+combine = undefined
+
+-- Return a list of the index along with each character
+enumerate :: [Char] -> [(Int, Char)]
+enumerate = undefined
+
+factorial :: Int -> Int
+factorial = undefined
