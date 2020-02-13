e328174adf19c3b0ffb69d5d3ef860ddb5110a44
Rudi B. Svartveit <rudsvar@gmail.com>
2020-02-13 15:51:22 +0100

    Run formatter

--- monday/CommonTypes.hs
+++ monday/CommonTypes.hs
@@ -94,7 +94,7 @@ helloWorld :: String
 helloWorld = "Hello " ++ "world!"
 
 alphabet :: String
-alphabet = ['a'..'z']
+alphabet = ['a' .. 'z']
 
 oneToTen :: [Int]
 oneToTen = [1 .. 10]
@@ -102,7 +102,7 @@ oneToTen = [1 .. 10]
 -- Tasks
 
 -- absolute value of an int
-absolute :: Int -> Int 
+absolute :: Int -> Int
 absolute x = if x < 0 then -x else x
 
 -- int with smallest value
@@ -111,8 +111,8 @@ smallest x y = if x < y then x else y
 
 -- incrementAllBy k xs means increment all elements by k
 incrementAllBy :: Int -> [Int] -> [Int]
-incrementAllBy _ [] = []
-incrementAllBy n (x:xs) = x+n : incrementAllBy n xs
+incrementAllBy _ []       = []
+incrementAllBy n (x : xs) = x + n : incrementAllBy n xs
 
 -- change the tuple structure
 changeTuple :: (Int, (Char, String)) -> ((Char, Int), String)
@@ -124,12 +124,12 @@ changeTuple (i, (c, s)) = ((c, i), s)
 -- "Buzz" if divisible by 5
 -- `show x` otherwise
 fizzBuzz :: [Int] -> [String]
-fizzBuzz [] = []
-fizzBuzz (x:xs) =
-    if mod x 3 == 0 && mod x 5 == 0 then "FizzBuzz" : fizzBuzz xs
-        else if mod x 3 == 0        then "Fizz" : fizzBuzz xs
-            else if mod x 5 == 0    then "Buzz" : fizzBuzz xs
-                else show x : fizzBuzz xs
+fizzBuzz []       = []
+fizzBuzz (x : xs) = if mod x 3 == 0 && mod x 5 == 0
+  then "FizzBuzz" : fizzBuzz xs
+  else if mod x 3 == 0
+    then "Fizz" : fizzBuzz xs
+    else if mod x 5 == 0 then "Buzz" : fizzBuzz xs else show x : fizzBuzz xs
 -- Alternative solution
 -- fizzBuzz str = map fizzOrBuzz str
 --     where
@@ -142,9 +142,10 @@ fizzBuzz (x:xs) =
 
 isAlphabetic :: Char -> Bool
 isAlphabetic c =
-    let isLower = 'a' <= c && c <= 'z'
-        isUpper = 'A' <= c && c <= 'Z'
-     in isLower || isUpper
+  let
+    isLower = 'a' <= c && c <= 'z'
+    isUpper = 'A' <= c && c <= 'Z'
+  in isLower || isUpper
 -- Alternative solution
 -- isAlphabetic c = c `elem` ['a'..'Z'] || c `elem` ['A' .. 'Z']
 
@@ -153,13 +154,13 @@ substring :: Int -> Int -> String -> String
 substring start end str = take (end - start) $ drop start str
 
 combine :: [Int] -> [Char] -> [(Int, Char)]
-combine [] _ = []
-combine _ [] = []
-combine (x:xs) (y:ys) = (x,y) : combine xs ys
+combine []       _        = []
+combine _        []       = []
+combine (x : xs) (y : ys) = (x, y) : combine xs ys
 
 enumerate :: [Char] -> [(Int, Char)]
-enumerate chars = combine [0..length chars-1] chars
+enumerate chars = combine [0 .. length chars - 1] chars
 
 factorial :: Int -> Int
 factorial 0 = 1
-factorial n = n * factorial (n-1)
+factorial n = n * factorial (n - 1)
